d2a6b9d1bef6e7db06ca2fe875e6b9c4bed18be1
sanyam810 <sanyamsaini081@gmail.com>
2023-06-09 03:52:07 +0530

    ts particles implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-
+import React from 'react';
 import Navbar from './components/Navbar';
-
 import SearchP from './components/SearchP';
+import Upload from './components/Upload';
+import Particles from './components/Particles';
 
-const App=()=> {
+
+const App = () => {
   return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        
-        <Navbar/>
-        <SearchP/>
-      </div>
+    <div>
+      <Navbar />
+      <Particles/>
 
+      
+      <div className="flex justify-center items-start min-h-screen bg-black py-40">
+        <div className="flex">
+          <div className='mr-48'>
+            <SearchP />
+          </div>
+          <div>
+            <Upload />
+            </div>
+        </div>
+      </div>
     </div>
-    
   );
-}
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="bg-black">
+    <nav className="w-full flex md:justify-center justify-between items-center p-4 bg-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-center h-24">
           <img src={logo} alt="BioCrawl Logo" className="h-24" />
